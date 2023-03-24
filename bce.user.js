e0a0688391ffb36bc2f2ffa311fa873a03a427cd
Sidsel <9719080-Sidiousious@users.noreply.gitlab.com>
2023-03-24 19:23:56 +0200

    fix posing for r90

--- bce.user.js
+++ bce.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name Bondage Club Enhancements
 // @namespace https://www.bondageprojects.com/
-// @version 4.23
+// @version 4.24
 // @description FBC - For Better Club - enhancements for the bondage club - old name kept in tampermonkey for compatibility
 // @author Sidious
 // @match https://bondageprojects.elementfx.com/*
@@ -39,19 +39,19 @@
  *  along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-const FBC_VERSION = "4.23";
+const FBC_VERSION = "4.24";
 const settingsVersion = 44;
 
 const fbcChangelog = `${FBC_VERSION}
+- fixed an error in anti-cheat
+- fixed being helped stand up
+
+4.23
 - R90 compatibility
 - fixed being helped to kneel or stand up
 
 4.22
 - R90 beta compatibility beta
-
-4.21
-- added MBS loader
-- stable BCX update
 `;
 
 /*
@@ -3664,8 +3664,7 @@ async function ForBetterClub() {
 			const time = Date.now();
 			// Deep copy
 			/** @type {ExpressionEvent} */
-			// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-			const event = JSON.parse(JSON.stringify(evt));
+			const event = deepCopy(evt);
 			event.At = time;
 			event.Until = time + event.Duration;
 			event.Id = newUniqueId();
@@ -5007,6 +5006,36 @@ async function ForBetterClub() {
 			},
 		});
 
+		/**
+		 * @param {string[]} poses
+		 */
+		function setPoses(poses) {
+			bceExpressionsQueue.forEach((e) => {
+				if (e.Type === MANUAL_OVERRIDE_EVENT_TYPE) {
+					e.Poses = [];
+				} else if (e.Poses?.length > 0) {
+					e.Poses.forEach((p) => {
+						if (p.Pose.length === 0) {
+							return;
+						}
+						if (typeof p.Pose[0] === "string") {
+							return;
+						}
+						/** @type {PoseEx[]} */
+						// @ts-ignore
+						const poseList = p.Pose;
+						p.Pose = poseList.filter((pp) => pp.Category);
+					});
+				}
+			});
+			const poseNames = PoseFemale3DCG.filter((p) =>
+				poses.includes(p.Name.toLowerCase())
+			).map((p) => p.Name);
+			for (const poseName of poseNames) {
+				CharacterSetActivePose(Player, poseName, false);
+			}
+		}
+
 		Commands.push({
 			Tag: "pose",
 			Description: displayText("['list' or list of poses]: set your pose"),
@@ -5031,30 +5060,7 @@ async function ForBetterClub() {
 						)
 					);
 				}
-				bceExpressionsQueue.forEach((e) => {
-					if (e.Type === MANUAL_OVERRIDE_EVENT_TYPE) {
-						e.Poses = [];
-					} else if (e.Poses?.length > 0) {
-						e.Poses.forEach((p) => {
-							if (p.Pose.length === 0) {
-								return;
-							}
-							if (typeof p.Pose[0] === "string") {
-								return;
-							}
-							/** @type {PoseEx[]} */
-							// @ts-ignore
-							const poseList = p.Pose;
-							p.Pose = poseList.filter((pp) => pp.Category);
-						});
-					}
-				});
-				const poseNames = PoseFemale3DCG.filter((p) =>
-					poses.includes(p.Name.toLowerCase())
-				).map((p) => p.Name);
-				for (const poseName of poseNames) {
-					CharacterSetActivePose(Player, poseName, false);
-				}
+				setPoses(poses);
 			},
 		});
 
@@ -5156,16 +5162,7 @@ async function ForBetterClub() {
 				}
 				if (data.MemberNumber === Player.MemberNumber) {
 					const poses = Array.isArray(data.Pose) ? data.Pose : [data.Pose];
-					pushEvent({
-						Type: MANUAL_OVERRIDE_EVENT_TYPE,
-						Duration: -1,
-						Poses: [
-							{
-								Duration: -1,
-								Pose: poses,
-							},
-						],
-					});
+					setPoses(poses);
 				}
 			}
 		);
@@ -5186,16 +5183,7 @@ async function ForBetterClub() {
 					const poses = Array.isArray(data.Character.ActivePose)
 						? data.Character.ActivePose
 						: [data.Character.ActivePose];
-					pushEvent({
-						Type: MANUAL_OVERRIDE_EVENT_TYPE,
-						Duration: -1,
-						Poses: [
-							{
-								Duration: -1,
-								Pose: poses,
-							},
-						],
-					});
+					setPoses(poses);
 				}
 			}
 		);
@@ -5602,12 +5590,9 @@ async function ForBetterClub() {
 				};
 			}
 			const basePoseMatcher = /^Base(Lower|Upper)$/u;
-			let newPose = Object.values(desiredPose)
+			const newPose = Object.values(desiredPose)
 				.map((p) => p.Pose)
 				.filter((p) => !basePoseMatcher.test(p));
-			if (newPose.length === 0) {
-				newPose = null;
-			}
 			if (JSON.stringify(Player.ActivePose) !== JSON.stringify(newPose)) {
 				Player.ActivePose = newPose;
 				needsPoseUpdate = true;
@@ -7323,17 +7308,20 @@ async function ForBetterClub() {
 
 			/** @type {(item: ItemBundle) => ItemBundle} */
 			function deleteUnneededMetaData(item) {
-				if (ignoreLocks && item?.Property) {
-					delete item.Property.LockMemberNumber;
-					delete item.Property.LockedBy;
-					delete item.Property.RemoveTimer;
-					delete item.Property.Effect;
+				if (!item) {
+					return item;
+				}
+				const clone = deepCopy(item);
+				if (ignoreLocks && clone?.Property) {
+					delete clone.Property.LockMemberNumber;
+					delete clone.Property.LockedBy;
+					delete clone.Property.RemoveTimer;
+					delete clone.Property.Effect;
 				}
 				if (ignoreColors) {
-					delete item.Color;
+					delete clone.Color;
 				}
-				// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-				return JSON.parse(JSON.stringify(item));
+				return clone;
 			}
 
 			function validateMistressLocks() {
@@ -9777,8 +9765,8 @@ async function ForBetterClub() {
 
 	/** @type {<T>(o: T) => T} */
 	function deepCopy(o) {
-		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
-		return JSON.parse(JSON.stringify(o));
+		// eslint-disable-next-line
+		return structuredClone(o);
 	}
 
 	// Confirm leaving the page to prevent accidental back button, refresh, or other navigation-related disruptions
